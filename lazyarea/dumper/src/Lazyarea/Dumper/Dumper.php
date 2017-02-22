116154248e880d7014177708d6f115e9209b77bf
vagrant <vagrant@ec.example.com>
2017-02-22 06:41:59 +0000

    Initial commit

--- lazyarea/dumper/src/Lazyarea/Dumper/Dumper.php
+++ lazyarea/dumper/src/Lazyarea/Dumper/Dumper.php
@@ -11,14 +11,17 @@ class Dumper
 
     public function dumper($str,$data=[]){
         $dt = date('Y-m-d G:i:s');
-        error_log(print_r("\n[$dt]",true),3,$this->fpath);
+        // logging timestamp
+        error_log(print_r("\n[$dt] ",true),3,$this->fpath);
+        // logging string part
         if ( !(string)$str ){
-            error_log(" [param0 is not string] []",3,$this->fpath);
+            error_log("[param0 is not string] []",3,$this->fpath);
             return;
         }
-        error_log(" [$str]",3,$this->fpath);
+        error_log("[$str]",3,$this->fpath);
+        // logging data part
         if ( gettype($data) == 'string' ){
-            error_log(" [$data]",3,$this->fpath);
+            error_log("[$data]",3,$this->fpath);
         }
         elseif ( is_array($data) ) {
           error_log(print_r($data,true),3,$this->fpath);
@@ -27,6 +30,4 @@ class Dumper
           error_log(print_r($data,true),3,$this->fpath);
         }
     }
-
-
 }
